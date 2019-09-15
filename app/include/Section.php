13b4761e547ccda6f29aa9d0af057e4d2d3b78d0
Choozy96 <47892948+Choozy96@users.noreply.github.com>
2019-09-15 13:16:11 +0800

    Added deleteAll and add methods for section DAO
    Added deleteAll and add methods for section DAO
    Renamed section object variables for consistency

--- app/include/Section.php
+++ app/include/Section.php
@@ -4,18 +4,18 @@ class Section {
     public $course;
     public $section;
     public $day;    
-    public $start_time;
-    public $end_time;
+    public $start;
+    public $end;
     public $instructor;
     public $venue;
     public $size;
     
-    public function __construct($course, $section, $day, $start_time, $end_time, $instructor, $venue, $size) {
+    public function __construct($course, $section, $day, $start, $end, $instructor, $venue, $size) {
         $this->course = $course;
         $this->section = $section;
         $this->day = $day;
-        $this->start_time = $start_time;
-        $this->end_time = $end_time;
+        $this->start = $start;
+        $this->end = $end;
         $this->instructor = $instructor;
         $this->venue = $venue;
         $this->size = $size;
--- app/include/SectionDAO.php
+++ app/include/SectionDAO.php
@@ -23,4 +23,38 @@ class SectionDAO {
                  
         return $result;
     }
+
+    public function deleteAll(){
+        $sql = 'TRUNCATE TABLE section';
+
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $count = $stmt->rowCount();
+
+    }
+
+    public function add($section){
+        //takes in section object
+        $sql = 'INSERT IGNORE into section(course, section, day, start, end, instructor, venue, size) values (:course, :section, :day, :start, :end, :instructor, :venue, :size)';
+
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':course', $section->course, PDO::PARAM_STR);
+        $stmt->bindParam(':section', $section->section, PDO::PARAM_STR);
+        $stmt->bindParam(':day', $section->day, PDO::PARAM_INT);
+        $stmt->bindParam(':start', $section->start, PDO::PARAM_STR);
+        $stmt->bindParam(':end', $section->end, PDO::PARAM_STR);
+        $stmt->bindParam(':instructor', $section->instructor, PDO::PARAM_STR);
+        $stmt->bindParam(':venue', $section->venue, PDO::PARAM_STR);
+        $stmt->bindParam(':size', $section->size, PDO::PARAM_INT);
+
+        $stmt->execute();
+    }
 }
