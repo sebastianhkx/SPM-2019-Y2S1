4c5be2dcee65c6b92e5c1accbd67c795f3b60aa2
Choozy96 <47892948+Choozy96@users.noreply.github.com>
2019-09-15 14:30:58 +0800

    created bootstrap.php

---
+++ app/include/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'common.php';
+
+function doBootstrap() {
+		
+
+	$errors = array();
+	# need tmp_name -a temporary name create for the file and stored inside apache temporary folder- for proper read address
+	$zip_file = $_FILES["bootstrap-file"]["tmp_name"];
+
+	# Get temp dir on system for uploading
+    $temp_dir = sys_get_temp_dir();
+    
+    if ($_FILES["bootstrap-file"]["size"] <= 0)
+        $errors[] = "input files not found";
+        #checks if zip file is empty
+
+	else {
+		
+		$zip = new ZipArchive;
+		$res = $zip->open($zip_file);
+
+		if ($res === TRUE) {
+			$zip->extractTo($temp_dir);
+            $zip->close();
+            
+            #sets path for the csv files
+            $student_path = "$temp_dir/student.csv";
+            $course_path = "$temp_dir/course.csv";
+            $section_path = "$temp_dir/section.csv";
+            $prerequisite_path = "$temp_dir/prerequisite.csv";
+            $course_completed_path = "$temp_dir/course_completed.csv";
+            $bid_path = "$temp_dir/bid.csv";
+
+
+
+
+?>
