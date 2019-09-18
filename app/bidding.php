c0f64c24e9e4a97b56af62864a7efa40fd7db346
mauriceljx <54699961+mauriceljx@users.noreply.github.com>
2019-09-18 11:56:58 +0800

    Added entry fields for bidding
    Current bid status is placeholder for now.

--- app/bidding.php
+++ app/bidding.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'include/common.php';
 
-echo "Your info<br>";
+echo "<h2>Your info:</h2>";
 
 $userid = $_SESSION['userid'];
 
@@ -19,14 +19,14 @@ echo "<table border=1>
     $result->school
     </td></tr>
 
-    <tr><th>E-dollar Balance</th>
+    <tr><th>e$ Balance</th>
     <td>
     $result->edollar
     </td></tr>
 
     </table><br>";
 
-echo "Your current bids<br>";
+echo "<h2>Your current bids:</h2>";
 
 $bid_dao = new BidDAO();
 $bids = $bid_dao->retrieveByUser($userid);
@@ -38,6 +38,7 @@ echo "<table border='1'>
         <th>Amount</th>
         <th>Course</th>
         <th>Section</th>
+        <th>Status</th>
     </tr>";
 
 for ($i = 1; $i <= count($bids); $i++) {
@@ -49,11 +50,28 @@ for ($i = 1; $i <= count($bids); $i++) {
         <td>$bid->amount</td>
         <td>$bid->code</td>
         <td>$bid->section</td>
+        <td>Placeholder</td>
     </tr>";
 }
 
 echo "</table>";
 
-
-echo "<a href='DisplayCourses.php' target='_blank' >Click to see all courses</a>";
 ?>
+
+<html>
+<body>
+
+<br>
+
+<h2>I want to bid for:</h2>
+<form action="process_bids.php" method="POST">
+Course: <input type="text" name="Course"> <br>
+Section: <input type="text" name="Section"> <br>
+Bid Amount: <input type="text" name="Bid Amount"> <br>
+
+<br>
+
+    <a href='DisplayCourses.php' target='_blank' >Click to see all courses</a>
+
+</body>
+</html>
