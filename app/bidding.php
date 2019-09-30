d93f0707b18ee0c3bc30a05d10e5fcb83cb81c47
Acancancan792 <54699980+Acancancan792@users.noreply.github.com>
2019-09-30 16:01:23 +0800

    Added UI for drop bid

--- app/bidding.php
+++ app/bidding.php
@@ -27,87 +27,169 @@ require_once 'include/common.php';
 </nav>
 
 <?php
-echo "<h2>Your info:</h2>";
 
-$userid = $_SESSION['userid'];
+  // Displays the current active round
+  $roundstatus_dao = new RoundStatusDAO();
+  $round_status = $roundstatus_dao->retrieveCurrentActiveRound();
+  if ($round_status != null) {
+    echo "<h1>Current Round: $round_status->round_num</h1>";
+  }
+  else {
+    echo "<h1>No active bidding round currently.</h1>";
+  }
+  echo "<hr>";
 
-$student_dao = new StudentDAO();
-$bid_dao = new BidDAO();
+  $userid = $_SESSION['userid'];
 
-if ( isset($_POST['submit'])){
-    $bidded = new Bid($userid, $_POST['bidamount'], $_POST['course'], $_POST['section']);
+  $student_dao = new StudentDAO();
+  $bid_dao = new BidDAO();
+
+  $display = "";
+  // if user wants to add a bid
+  if (isset($_POST['see_add_bid'])) {
+    $course = '';
+    $section = '';
+    $amount = '';
+    $display = '<h2>I want to bid for:</h2>
+    <form action="bidding.php" method="POST">
+    Course: <input type="text" name="course" required> <br>
+    Section: <input type="text" name="section" required> <br>
+    Bid Amount: <input type="number" name="bidamount" placeholder="1.00" step="0.01" min="10.00" required> <br>
+    <input type="submit" name="submitbid" value="Confirm Bid" >';
+  }
+
+  // if user wants to drop a bid
+  if (isset($_POST['see_drop_bid'])) {
+    $coursedrop = '';
+    $sectiondrop = '';
+    $display = '<h2>I want to drop this bid:</h2>
+    <form action="bidding.php" method="POST">
+    Course: <input type="text" name="coursedrop" required> <br>
+    Section: <input type="text" name="sectiondrop" required> <br>
+    <input type="submit" name="submitdrop" value="Drop Bid" >';
+  }
+
+  $student = $student_dao->retrieve($userid); // student object
+  $bids = $bid_dao->retrieveByUser($userid); // could be an array of bids
+
+  echo "<h2>Your info:</h2>";
+  echo "<table border=1>
+      <tr>
+          <th>Name</th>
+          <td>$student->name</td>
+      </tr>  
+      <tr>
+          <th>School</th>
+          <td>$student->school</td>
+      </tr>
+      <tr>
+          <th>e$ Balance</th>
+          <td>$student->edollar</td>
+      </tr>
+      </table><hr>";
+
+  echo "<h2>Your current bids:</h2>";
+
+  echo "<table border='1'>
+      <tr>
+          <th>No.</th>
+          <th>User ID</th>
+          <th>Amount</th>
+          <th>Course</th>
+          <th>Section</th>
+          <th>Status</th>
+      </tr>";
+
+  for ($i = 1; $i <= count($bids); $i++) {
+      $bid = $bids[$i-1];
+      echo "
+      <tr>
+          <td>$i</td>
+          <td>$bid->userid</td>
+          <td>$bid->amount</td>
+          <td>$bid->course</td>
+          <td>$bid->section</td>
+          <td>Placeholder</td>
+      </tr>";
+  }
+
+  echo "</table><hr>";
+
+  // if user submits a new bid
+  if (isset($_POST['submitbid'])) {
+    $course = $_POST['course']; // for repopulating form fields also
+    $section = $_POST['section'];
+    $amount = $_POST['bidamount'];
+
+    $bidded = new Bid($userid, $amount, $course, $section);
     $errors = $bid_dao->add($bidded);
-    var_dump($errors);
-}
+    if (is_array($errors)) {
+      echo "Errors:<br><ul>";
+      foreach($errors as $err) {
+        echo "<li>$err</li>";
+      }
+      echo "</ul>";
+    }
+    else {
+      echo "<h1>Bid was added successfully!</h1><br>
+            <h1>e$ updated</h1>";
+    }
+  }
 
-//$student_dao = new StudentDAO();
-$student = $student_dao->retrieve($userid); // student object
+  // if user drops a bid
+  if (isset($_POST['submitdrop'])){
+    $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
+    $sectiondrop = $_POST['sectiondrop'];
 
-//$bid_dao = new BidDAO();
-$bids = $bid_dao->retrieveByUser($userid); // could be an array of bids
-
-echo "<table border=1>
-    <tr>
-        <th>Name</th>
-        <td>$student->name</td>
-    </tr>  
-    <tr>
-        <th>School</th>
-        <td>$student->school</td>
-    </tr>
-    <tr>
-        <th>e$ Balance</th>
-        <td>$student->edollar</td>
-    </tr>
-    </table><br>";
-
-echo "<h2>Your current bids:</h2>";
-
-echo "<table border='1'>
-    <tr>
-        <th>No.</th>
-        <th>User ID</th>
-        <th>Amount</th>
-        <th>Course</th>
-        <th>Section</th>
-        <th>Status</th>
-        <th>Refund</th>
-    </tr>";
-
-for ($i = 1; $i <= count($bids); $i++) {
-    $bid = $bids[$i-1];
-    echo "
-    <tr>
-        <td>$i</td>
-        <td>$bid->userid</td>
-        <td>$bid->amount</td>
-        <td>$bid->course</td>
-        <td>$bid->section</td>
-        <td>Placeholder</td>
-        <td>Placeholder</td>
-    </tr>";
-}
-
-echo "</table>";
+    $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
+    $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
+    $errors = $bid_dao->drop($bid_to_drop);
+    if (is_array($errors)) {
+      echo "Errors:<br><ul>";
+      foreach($errors as $err) {
+        echo "<li>$err</li>";
+      }
+      echo "</ul>";
+    }
+    else {
+      echo "<h1>Bid was dropped successfully!</h1><br>
+            <h1>e$ updated</h1>";
+    }
+  }
 
 ?>
 
 <html>
 <body>
 
-<br>
+  <h3>Choose an option:</h3>
+  <form action="bidding.php" method="POST">
+    <input type="submit" value="I want to add a bid" name="see_add_bid">
+    <input type="submit" value="I want to drop a bid" name="see_drop_bid">
+  </form>
 
-<h2>I want to bid for:</h2>
-<form action="bidding.php" method="POST">
-Course: <input type="text" name="course" required> <br><br>
-Section: <input type="text" name="section" required> <br><br>
-Bid Amount: <input type="number" name="bidamount" placeholder="1.00" step="0.01" min="10.00"required> <br><br>
-<input type="submit" name='submit' value="Confirm Bid" >
+  <?php
+    echo $display;
+  ?>
 
-<br>
+  <!-- <h2>I want to bid for:</h2>
+  <form action="bidding.php" method="POST">
+  Course: <input type="text" name="course" value="<?= $course ?>" required> <br>
+  Section: <input type="text" name="section" value="<?= $section ?>" required> <br>
+  Bid Amount: <input type="number" name="bidamount" placeholder="1.00" step="0.01" min="10.00" value="<?= $amount ?>" required> <br>
+  <input type="submit" name='submitbid' value="Confirm Bid" > -->
+
+  <!-- <h2>I want to drop this bid :</h2>
+  <form action="bidding.php" method="POST">
+  Course: <input type="text" name="coursedrop" value="<?= $coursedrop ?>" required> <br>
+  Section: <input type="text" name="sectiondrop" value="<?= $sectiondrop ?>" required> <br>
+  <input type="submit" name='submitdrop' value="Drop Bid" > -->
+
+  <br>
+  <br>
 
     <a href='DisplayCourses.php' target='_blank' >Click to see all courses</a>
 
-<br>   
+  <br>   
 </body>
 </html>
