8d3da84f0386b727257ccbc1b73b682fd0bd27ec
mauriceljx <54699961+mauriceljx@users.noreply.github.com>
2019-09-30 17:23:05 +0800

    Changed DropBid layout
    Use input form fields instead of checkbox for now

--- app/DropBid.php
+++ app/DropBid.php
@@ -1,25 +1,27 @@
 <?php
     require_once 'include/common.php';
+
+    // Displays the current active round
+    $roundstatus_dao = new RoundStatusDAO();
+    $round_status = $roundstatus_dao->retrieveCurrentActiveRound();
+    if ($round_status != null) {
+        echo "<h1>Current Round: $round_status->round_num</h1>";
+    }
+    else {
+        echo "<h1>No active bidding round currently.</h1>";
+    }
+    echo "<hr>";
+
+    $coursedrop = '';
+    $sectiondrop = '';
+
     $userid = $_SESSION['userid'];
 
     $student_dao = new StudentDAO();
     $bid_dao = new BidDAO();
     $bids = $bid_dao->retrieveByUser($userid);
 
-    if(isset($_POST['submit'])){
-        if(isset($_POST['bid'])){
-            $index = $_POST['bid'];
-            $bid_drop = $bids[$index-1];
-            //var_dump($bid_drop);
-            $bid_dao->drop($bid_drop);
-            $student_dao->addEdollar($bid_drop->userid, $bid_drop->amount);
-        }
-    }
-
-    
     $student = $student_dao->retrieve($userid); // student object
-
-    
     $bids = $bid_dao->retrieveByUser($userid); // could be an array of bids
 
     echo "<h2>Your info:</h2>";
@@ -36,25 +38,20 @@
             <th>e$ Balance</th>
             <td>$student->edollar</td>
         </tr>
-        </table><br>";
-?>
-
-<html>
-<body>
-<form action="DropBid.php" method="POST">
-<h2>Your current bids:</h2>
-
-<table border='1'>
-    <tr>
-        <th>No.</th>
-        <th>User ID</th>
-        <th>Amount</th>
-        <th>Course</th>
-        <th>Section</th>
-        <th>Drop</th>
-    </tr>
-
-<?php
+        </table><hr>";
+  
+    echo "<h2>Your current bids:</h2>";
+  
+    echo "<table border='1'>
+        <tr>
+            <th>No.</th>
+            <th>User ID</th>
+            <th>Amount</th>
+            <th>Course</th>
+            <th>Section</th>
+            <th>Status</th>
+        </tr>";
+  
     for ($i = 1; $i <= count($bids); $i++) {
         $bid = $bids[$i-1];
         echo "
@@ -64,18 +61,40 @@
             <td>$bid->amount</td>
             <td>$bid->course</td>
             <td>$bid->section</td>
-            <td><input type='radio' name='bid' value='$i'></td>
+            <td>Placeholder</td>
         </tr>";
     }
+  
+    echo "</table><hr>";
+
+    // if user drops a bid
+    if (isset($_POST['submitdrop'])){
+        $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
+        $sectiondrop = $_POST['sectiondrop'];
+
+        $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
+        $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
+        $errors = $bid_dao->drop($bid_to_drop);
+        if (is_array($errors)) {
+        echo "Errors:<br><ul>";
+        foreach($errors as $err) {
+            echo "<li>$err</li>";
+        }
+        echo "</ul>";
+        }
+        else {
+        echo "<h1>Bid was dropped successfully!</h1><br>
+                <h1>e$ updated</h1>";
+        }
+    }
 ?>
-</table>
 
-    <br>
-    <input type="submit" name='submit' value="Confirm Drop" >
-    <br>
-        <a href='DisplayCourses.php' target='_blank' >Click to see all courses</a>
-
-    <br>  
-    <a href='logout.php'>Log Out</a> 
+<html>
+<body>
+    <h2>I want to drop this bid :</h2>
+    <form action="DropBid.php" method="POST">
+    Course: <input type="text" name="coursedrop" value="<?= $coursedrop ?>" required> <br>
+    Section: <input type="text" name="sectiondrop" value="<?= $sectiondrop ?>" required> <br>
+    <input type="submit" name='submitdrop' value="Drop Bid" >
 </body>
 </html>
