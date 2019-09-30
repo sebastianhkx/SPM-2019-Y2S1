27c275f37a929c65cb63d072f17c3578b01ece6c
Acancancan792 <54699980+Acancancan792@users.noreply.github.com>
2019-09-30 17:21:42 +0800

    Reorder retrieve and add
    Page not updating because wrong order

--- app/bidding.php
+++ app/bidding.php
@@ -69,6 +69,48 @@ require_once 'include/common.php';
     <input type="submit" name="submitdrop" value="Drop Bid" >';
   }
 
+    // if user submits a new bid
+    if (isset($_POST['submitbid'])) {
+      $course = $_POST['course']; // for repopulating form fields also
+      $section = $_POST['section'];
+      $amount = $_POST['bidamount'];
+  
+      $bidded = new Bid($userid, $amount, $course, $section);
+      $errors = $bid_dao->add($bidded);
+      if (is_array($errors)) {
+        echo "Errors:<br><ul>";
+        foreach($errors as $err) {
+          echo "<li>$err</li>";
+        }
+        echo "</ul>";
+      }
+      else {
+        echo "<h1>Bid was added successfully!</h1><br>
+              <h1>e$ updated</h1>";
+      }
+    }
+
+    // if user drops a bid
+    if (isset($_POST['submitdrop'])){
+      $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
+      $sectiondrop = $_POST['sectiondrop'];
+  
+      $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
+      $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
+      $errors = $bid_dao->drop($bid_to_drop);
+      if (is_array($errors)) {
+        echo "Errors:<br><ul>";
+        foreach($errors as $err) {
+          echo "<li>$err</li>";
+        }
+        echo "</ul>";
+      }
+      else {
+        echo "<h1>Bid was dropped successfully!</h1><br>
+              <h1>e$ updated</h1>";
+      }
+    }
+
   $student = $student_dao->retrieve($userid); // student object
   $bids = $bid_dao->retrieveByUser($userid); // could be an array of bids
 
@@ -114,48 +156,6 @@ require_once 'include/common.php';
   }
 
   echo "</table><hr>";
-
-  // if user submits a new bid
-  if (isset($_POST['submitbid'])) {
-    $course = $_POST['course']; // for repopulating form fields also
-    $section = $_POST['section'];
-    $amount = $_POST['bidamount'];
-
-    $bidded = new Bid($userid, $amount, $course, $section);
-    $errors = $bid_dao->add($bidded);
-    if (is_array($errors)) {
-      echo "Errors:<br><ul>";
-      foreach($errors as $err) {
-        echo "<li>$err</li>";
-      }
-      echo "</ul>";
-    }
-    else {
-      echo "<h1>Bid was added successfully!</h1><br>
-            <h1>e$ updated</h1>";
-    }
-  }
-
-  // if user drops a bid
-  if (isset($_POST['submitdrop'])){
-    $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
-    $sectiondrop = $_POST['sectiondrop'];
-
-    $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
-    $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
-    $errors = $bid_dao->drop($bid_to_drop);
-    if (is_array($errors)) {
-      echo "Errors:<br><ul>";
-      foreach($errors as $err) {
-        echo "<li>$err</li>";
-      }
-      echo "</ul>";
-    }
-    else {
-      echo "<h1>Bid was dropped successfully!</h1><br>
-            <h1>e$ updated</h1>";
-    }
-  }
 
 ?>
 
