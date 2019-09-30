36d65817e3245c8f9263e64241cb4fbf784b55b5
sebastianhkx <kxhong.2018@sis.smu.edu.sg>
2019-09-30 17:39:32 +0800

    changed displaycourses table formatting

--- app/DisplayCourses.php
+++ app/DisplayCourses.php
@@ -1,7 +1,24 @@
 <?php
 
 require_once 'include/common.php';
+?>
+<html>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
 
+        th, td {
+            border: 1px solid black;
+            text-align: center;
+        }
+        
+        th {
+        padding: 10px;
+        }
+    </style>
+</html>
+<?php
 $dao = new CourseDAO();
 $courses = $dao->retrieveAll();
 
@@ -15,7 +32,7 @@ echo "<table border='1'>
         <th>Exam Date</th>
         <th>Exam Start Time</th>
         <th>Exam End time</th>
-        <th>
+        
     </tr>";
 
 for ($i = 1; $i <= count($courses); $i++) {
@@ -26,7 +43,7 @@ for ($i = 1; $i <= count($courses); $i++) {
         <td>$course->course</td>
         <td>$course->school</td>
         <td>$course->title</td>
-        <td>$course->description</td>
+        <td style='text-align:left' width='500'>$course->description</td>
         <td>$course->exam_date</td>
         <td>$course->exam_start</td>
         <td>$course->exam_end</td>
