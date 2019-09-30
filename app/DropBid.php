8ce8ae13308df9da4614376e312be6e071693ed9
mauriceljx <54699961+mauriceljx@users.noreply.github.com>
2019-09-30 18:23:39 +0800

    Adding and Dropping bids separated into two different pages now
    Cleaner UI, and we can repopulate the form fields now.

--- app/DropBid.php
+++ app/DropBid.php
@@ -1,6 +1,32 @@
 <?php
     require_once 'include/common.php';
+?>
 
+<html>
+<head>
+  <title>BIOS Home</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand">BIOS</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="home.php">Home</a></li>
+      <li><a href="bidding.php">Bidding</a></li>
+      <li class="active"><a href='DropBid.php'>Drop Bid</a></li>
+      <li><a href='logout.php'>Log Out</a></li>
+    </ul>
+  </div>
+</nav>
+
+<?php
     // Displays the current active round
     $roundstatus_dao = new RoundStatusDAO();
     $round_status = $roundstatus_dao->retrieveCurrentActiveRound();
@@ -19,7 +45,27 @@
 
     $student_dao = new StudentDAO();
     $bid_dao = new BidDAO();
-    $bids = $bid_dao->retrieveByUser($userid);
+
+    // if user drops a bid
+    if (isset($_POST['submitdrop'])){
+        $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
+        $sectiondrop = $_POST['sectiondrop'];
+
+        $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
+        $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
+        $errors = $bid_dao->drop($bid_to_drop);
+        if (is_array($errors)) {
+        echo "Errors:<br><ul>";
+        foreach($errors as $err) {
+            echo "<li>$err</li>";
+        }
+        echo "</ul>";
+        }
+        else {
+        echo "<h1>Bid was dropped successfully!</h1><br>
+                <h1>e$ updated</h1>";
+        }
+    }    
 
     $student = $student_dao->retrieve($userid); // student object
     $bids = $bid_dao->retrieveByUser($userid); // could be an array of bids
@@ -66,27 +112,6 @@
     }
   
     echo "</table><hr>";
-
-    // if user drops a bid
-    if (isset($_POST['submitdrop'])){
-        $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
-        $sectiondrop = $_POST['sectiondrop'];
-
-        $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
-        $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
-        $errors = $bid_dao->drop($bid_to_drop);
-        if (is_array($errors)) {
-        echo "Errors:<br><ul>";
-        foreach($errors as $err) {
-            echo "<li>$err</li>";
-        }
-        echo "</ul>";
-        }
-        else {
-        echo "<h1>Bid was dropped successfully!</h1><br>
-                <h1>e$ updated</h1>";
-        }
-    }
 ?>
 
 <html>
--- app/bidding.php
+++ app/bidding.php
@@ -18,9 +18,8 @@ require_once 'include/common.php';
     </div>
     <ul class="nav navbar-nav">
       <li><a href="home.php">Home</a></li>
-      
       <li class="active"><a href="bidding.php">Bidding</a></li>
-      <li><a href='dropbid.php'>Drop Bid</a></li>
+      <li><a href='DropBid.php'>Drop Bid</a></li>
       <li><a href='logout.php'>Log Out</a></li>
     </ul>
   </div>
@@ -44,72 +43,30 @@ require_once 'include/common.php';
   $student_dao = new StudentDAO();
   $bid_dao = new BidDAO();
 
-  $display = "";
-  // if user wants to add a bid
-  if (isset($_POST['see_add_bid'])) {
-    $course = '';
-    $section = '';
-    $amount = '';
-    $display = '<h2>I want to bid for:</h2>
-    <form action="bidding.php" method="POST">
-    Course: <input type="text" name="course" required> <br>
-    Section: <input type="text" name="section" required> <br>
-    Bid Amount: <input type="number" name="bidamount" placeholder="1.00" step="0.01" min="10.00" required> <br>
-    <input type="submit" name="submitbid" value="Confirm Bid" >';
-  }
+  $course = '';
+  $section = '';
+  $amount = '';
 
-  // if user wants to drop a bid
-  if (isset($_POST['see_drop_bid'])) {
-    $coursedrop = '';
-    $sectiondrop = '';
-    $display = '<h2>I want to drop this bid:</h2>
-    <form action="bidding.php" method="POST">
-    Course: <input type="text" name="coursedrop" required> <br>
-    Section: <input type="text" name="sectiondrop" required> <br>
-    <input type="submit" name="submitdrop" value="Drop Bid" >';
-  }
+  // if user submits a new bid
+  if (isset($_POST['submitbid'])) {
+    $course = $_POST['course']; // for repopulating form fields also
+    $section = $_POST['section'];
+    $amount = $_POST['bidamount'];
 
-    // if user submits a new bid
-    if (isset($_POST['submitbid'])) {
-      $course = $_POST['course']; // for repopulating form fields also
-      $section = $_POST['section'];
-      $amount = $_POST['bidamount'];
-  
-      $bidded = new Bid($userid, $amount, $course, $section);
-      $errors = $bid_dao->add($bidded);
-      if (is_array($errors)) {
-        echo "Errors:<br><ul>";
-        foreach($errors as $err) {
-          echo "<li>$err</li>";
-        }
-        echo "</ul>";
+    $bidded = new Bid($userid, $amount, $course, $section);
+    $errors = $bid_dao->add($bidded);
+    if (is_array($errors)) {
+      echo "Errors:<br><ul>";
+      foreach($errors as $err) {
+        echo "<li>$err</li>";
       }
-      else {
-        echo "<h1>Bid was added successfully!</h1><br>
-              <h1>e$ updated</h1>";
-      }
+      echo "</ul>";
     }
-
-    // if user drops a bid
-    if (isset($_POST['submitdrop'])){
-      $coursedrop = $_POST['coursedrop']; // for repopulating form fields also
-      $sectiondrop = $_POST['sectiondrop'];
-  
-      $bid_to_drop_temp = new Bid($userid, 0, $coursedrop, $sectiondrop); // the current drop bid method doesn't need amount. might need to revisit the method.
-      $bid_to_drop = new Bid($userid, $bid_dao->checkExistingBid($bid_to_drop_temp), $coursedrop, $sectiondrop);
-      $errors = $bid_dao->drop($bid_to_drop);
-      if (is_array($errors)) {
-        echo "Errors:<br><ul>";
-        foreach($errors as $err) {
-          echo "<li>$err</li>";
-        }
-        echo "</ul>";
-      }
-      else {
-        echo "<h1>Bid was dropped successfully!</h1><br>
-              <h1>e$ updated</h1>";
-      }
+    else {
+      echo "<h1>Bid was added successfully!</h1><br>
+            <h1>e$ updated</h1>";
     }
+  }
 
   $student = $student_dao->retrieve($userid); // student object
   $bids = $bid_dao->retrieveByUser($userid); // could be an array of bids
@@ -162,28 +119,12 @@ require_once 'include/common.php';
 <html>
 <body>
 
-  <h3>Choose an option:</h3>
-  <form action="bidding.php" method="POST">
-    <input type="submit" value="I want to add a bid" name="see_add_bid">
-    <input type="submit" value="I want to drop a bid" name="see_drop_bid">
-  </form>
-
-  <?php
-    echo $display;
-  ?>
-
-  <!-- <h2>I want to bid for:</h2>
+  <h2>I want to bid for:</h2>
   <form action="bidding.php" method="POST">
   Course: <input type="text" name="course" value="<?= $course ?>" required> <br>
   Section: <input type="text" name="section" value="<?= $section ?>" required> <br>
   Bid Amount: <input type="number" name="bidamount" placeholder="1.00" step="0.01" min="10.00" value="<?= $amount ?>" required> <br>
-  <input type="submit" name='submitbid' value="Confirm Bid" > -->
-
-  <!-- <h2>I want to drop this bid :</h2>
-  <form action="bidding.php" method="POST">
-  Course: <input type="text" name="coursedrop" value="<?= $coursedrop ?>" required> <br>
-  Section: <input type="text" name="sectiondrop" value="<?= $sectiondrop ?>" required> <br>
-  <input type="submit" name='submitdrop' value="Drop Bid" > -->
+  <input type="submit" name='submitbid' value="Confirm Bid" >
 
   <br>
   <br>
