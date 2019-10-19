91b54285f72a5f4dd197f9d05dcc29633ac50744
zychoosis <zychoo.2018@sis.smu.edu.sg>
2019-10-19 14:32:15 +0800

    Modifed and added protect_json to JSON files

--- app/include/protect_json.php
+++ app/include/protect_json.php
@@ -2,14 +2,27 @@
 require_once 'token.php';
 require_once 'common.php';
 
-$token = '';
-if  (isset($_REQUEST['token'])) {
-	$token = $_REQUEST['token'];
-}
 
-# check if token is not valid
-if (verify_token($token)==FALSE){
-	echo "token authentication failed";
+$token = '';
+header('Content-Type: application/json');
+if  (isset($_REQUEST['token'])) {
+	//token present
+	$token = $_REQUEST['token'];
+	if ($token == ''){
+		//token blank
+		echo json_encode(['status'=>'error', 'message'=>['blank token']],JSON_PRETTY_PRINT);
+		exit();
+	}
+	# check if token is not valid
+	elseif (verify_token($token)==FALSE){
+		echo json_encode(['status'=>'error', 'message'=>['invalid token']],JSON_PRETTY_PRINT);
+		exit();
+	}
+}
+else{
+	//token missing
+	echo json_encode(['status'=>'error', 'message'=>['missing token']],JSON_PRETTY_PRINT);
 	exit();
 }
+
 ?>
