71af1e24b48b54e046ddcbd321851569a4216090
Acancancan792 <54699980+Acancancan792@users.noreply.github.com>
2019-10-21 22:51:37 +0800

    Create course json dump function for Section.php

--- app/include/Section.php
+++ app/include/Section.php
@@ -20,6 +20,78 @@ class Section {
         $this->venue = $venue;
         $this->size = $size;
     }
+    public function getCourse(){
+        return $this->course;
+    }
+
+    public function getSection(){
+        return $this->section;
+    }
+
+    public function getDay(){
+        return $this->day;
+    }
+
+    public function getStart(){
+        return $this->start;
+    }
+    
+    public function getStartJSON(){
+        $var1=$this->start;
+        $fstart=strtotime($var1);
+        $var2=date('Hi',$fstart);
+        if($var1[0]=="0"){
+            $start=substr($var2,1,4);
+        }
+        else{
+            $start=substr($var2,0,5);
+        }
+        return $start;
+    }
+
+
+    public function getExamEndJSON(){
+        $var1=$this->exam_end;
+        $fexam_end=strtotime($var1);
+        $var2=date('Hi',$fexam_end);
+        if($var2[0]=="0"){
+            $exam_end=substr($var2,1,4);
+        }
+        else{
+            $exam_end=substr($var2,0,5);
+        }
+        return $exam_end;
+    }
+
+
+    public function getEnd(){
+        return $this->end;
+    }
+
+    public function getEndJSON(){
+        $var1=$this->end;
+        $fend=strtotime($var1);
+        $var2=date('Hi',$fend);
+        if($var1[0]=="0"){
+            $end=substr($var2,1,4);
+        }
+        else{
+            $end=substr($var2,0,5);
+        }
+        return $end;
+    }
+
+    public function getInstructor(){
+        return $this->instructor;
+    }
+
+    public function getVenue(){
+        return $this->venue;
+    }
+
+    public function getSize(){
+        return $this->size;
+    }
     
 }
 
